7ef5843ee070f65f3dbd0c5bed69e57aeb389d58
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-28 12:13:29 +0000

    Trabajando con VisuallyHidder.jsx con el hook useEffect()

--- src/componentes/VisuallyHidden/VisuallyHidden.jsx
+++ src/componentes/VisuallyHidden/VisuallyHidden.jsx
@@ -1,6 +1,10 @@
 import styles from "./VisuallyHidden.module.css";
+import { useState, useEffect } from "react";
 
 export default function VisuallyHidden({ children, ...delegated }) {
+  const [forceVisibility, setForceVisibility] = useState(false);
+
+  useEffect(() => {}, []);
   return (
     <div
       className={styles.wrapper}
