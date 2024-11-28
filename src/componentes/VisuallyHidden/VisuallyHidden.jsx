fccdb48ff84a1160208aed6725e4daec4285a372
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-28 12:11:14 +0000

    Trabajando con VisuallyHidder.jsx

---
+++ src/componentes/VisuallyHidden/VisuallyHidden.jsx
@@ -0,0 +1,12 @@
+import styles from "./VisuallyHidden.module.css";
+
+export default function VisuallyHidden({ children, ...delegated }) {
+  return (
+    <div
+      className={styles.wrapper}
+      {...delegated}
+    >
+      {children}
+    </div>
+  );
+}
