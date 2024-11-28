f0d8b8daccb940e61d49f04966750c7969383f40
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-28 14:28:29 +0000

    Trabajando con Hero.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import styles from "./page.module.css";
 import Header from "@/componentes/Header";
+import Hero from "@/componentes/Hero";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <>
+      <div className={styles.headerWrapper}>
         <Header />
+      </div>
+      <main className={styles.main}>
+        <Hero />
       </main>
-    </div>
+    </>
   );
 }
---
+++ src/componentes/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import styles from "./Hero.module.css";
+import Image from "next/image";
+import swoop from "../../../public/images/swoop.svg";
+import hero from "../../../public/images/hero-img.jpg";
+
+export default function Hero() {
+  return (
+    <section className={styles.wrapper}>
+      <Image
+        className={styles.heroImage}
+        src={hero}
+        alt="Hero"
+      />
+      <Image
+        className={styles.swoop}
+        src={swoop}
+        alt=""
+      />
+    </section>
+  );
+}
