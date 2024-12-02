c38c576429efbfc6ea5c97d8d33199c849090df3
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-02 14:27:13 +0000

    Trabajando en PhotoGridItem.module.css

--- src/componentes/PhotoGridItem/PhotoGridItem.jsx
+++ src/componentes/PhotoGridItem/PhotoGridItem.jsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./PhotoGridItem.module.css";
 
 export default function PhotoGridItem({ id, src, alt, tags }) {
   return (
     <article>
-      <Link href={`/photos/${id}`}>
+      <Link
+        href={`/photos/${id}`}
+        className={styles.a}
+      >
         <Image
+          className={styles.img}
           src={src}
           alt={alt}
           width={300}
           height={300}
+          priority
         />
       </Link>
-      <ul>
+      <ul className={styles.tags}>
         {tags.map((tag) => (
           <li key={tag}>{tag}</li>
         ))}
