e02f23a20e9249145c8acfe44fee6f3f93939f1b
catharygr <103951447+catharygr@users.noreply.github.com>
2024-12-04 11:21:01 +0000

    Trabajando en el componente Footer.module.css

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,7 +1,9 @@
+import styles from "./Footer.module.css";
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className={styles.footer}>
+      <p>© 2024-{year}. All rights reserved.</p>
     </footer>
   );
 }
